90e6addabfa10c2c1f47c320d246b02ddc10bcc2
cihattascigit config --global user.email cihattasci97@gmail.comgit config --global user.name cihattasci <63815355+cihattasci@users.noreply.github.com>
2021-06-17 21:47:42 +0300

    fixed

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ var PORT = process.env.PORT || 8080;
 var bodyParser = require('body-parser');
 var router = require('./API/Router/routes');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(router);
